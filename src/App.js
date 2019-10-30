f3ef0bfab307e191bfb63be1736bb70af47d1ed6
R0m3o <54901507+R0m3o@users.noreply.github.com>
2019-10-30 11:58:48 +0100

    pulled information from our API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    bruger: []
+  }
+
+  componentDidMount(){
+    axios.get("https://worldcruiseapi.azurewebsites.net/bruger")
+    .then(res => {
+        console.log(res.data)
+        this.setState({
+          bruger: res.data
+        });
+    });
+  }
+  
+  render(){
+    const {bruger} = this.state;
+    const brugerListe = bruger.length ? (
+      bruger.map(brug => {
+        return (
+          <div key={brug._id}>
+            <h5>{brug.Navn}</h5>
+            <p>{brug.Adresse}</p>
+            <p>{brug.Email}</p>
+            <p>{brug.Nationalitet}</p>
+          </div>
+        )
+      })
+    ) : (
+      <div>Loading</div>
+    )
+    return (
+      <div>
+        {brugerListe}
+      </div>
+    )
+  }
 }
 
 export default App;
